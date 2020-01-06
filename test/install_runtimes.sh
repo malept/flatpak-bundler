f22c66d2dc760742f98c9b7923de139ed182f97f
Mark Lee <electronjs@lazymalevolence.com>
2020-01-06 11:15:51 -0800

    build: update runtime installer

--- test/install_runtimes.sh
+++ test/install_runtimes.sh
@@ -3,13 +3,20 @@
 install_runtime() {
     local arch="$1"
     local runtime="$2"
-    local url="$3"
-    if [[ ! -d "$HOME/.local/share/flatpak/runtime/${runtime}.Locale/$arch" ]]; then
-        flatpak install --user --no-deps --assumeyes --arch $arch --from $url
+    local version="$3"
+    install_flatpak "runtime/${runtime}/$arch/$version"
+}
+
+install_flatpak() {
+    local ref="$1"
+    if [[ ! -d "$HOME/.local/share/flatpak/$ref" ]]; then
+        flatpak install --user --no-deps --assumeyes "$ref"
     fi
 }
 
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 for arch in i386 x86_64; do
-    install_runtime $arch org.freedesktop.Sdk https://raw.githubusercontent.com/endlessm/flatpak-bundler/master/refs/freedesktop-sdk-1.4.flatpakref
-    install_runtime $arch org.freedesktop.Platform https://raw.githubusercontent.com/endlessm/flatpak-bundler/master/refs/freedesktop-runtime-1.4.flatpakref
+    install_runtime $arch org.freedesktop.Sdk 1.4
+    install_runtime $arch org.freedesktop.Platform 1.4
 done
